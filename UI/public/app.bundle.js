a6d09a48d8397e9c57ad55f0aa635847a26362e6
Shailendra2995 <skarki5277@conestogac.on.ca>
2024-11-28 23:38:50 -0500

    checknow

--- UI/public/app.bundle.js
+++ UI/public/app.bundle.js
@@ -1736,7 +1736,6 @@ function ContactInfo(_ref6) {
     type: "invalid"
   }, formErrors.phone || "Please provide a valid phone number.")));
 }
-//checkout
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CheckoutPage);
 
 /***/ }),
